87794fe878d608d9beab6f230bdd290e639db1bf
nixandra14 <146364979+nixandra14@users.noreply.github.com>
2023-12-18 20:46:55 +0200

    Add files via upload

---
+++ Proiect2.Data/Models/PaymentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect2.Data.Models
+{
+    public class PaymentDto
+    {
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        public int Total { get; set; }
+        public string CardNumber { get; set; }
+
+    }
+}
---
+++ Proiect2.Data/PaymentsContext.cs
@@ -0,0 +1,24 @@
+﻿using Proiect2.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect2.Data
+{
+    public class PaymentsContext : DbContext
+    {
+        public PaymentsContext(DbContextOptions<PaymentsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDto> Bills { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PaymentDto>().ToTable("Payment").HasKey(s => s.PaymentId);
+        }
+    }
+}
